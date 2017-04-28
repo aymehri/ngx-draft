de3158c40ee58cdd2cde77a38243f9952f0744d4
Aymen Mehri <aymen.mehri@soat.fr>
2017-04-28 10:45:18 +0200

    add validation

--- ps-server/server/models/promosport.js
+++ ps-server/server/models/promosport.js
@@ -1,5 +1,11 @@
 'use strict';
 
-module.exports = function(Promosport) {
+module.exports = function (Promosport) {
+    Promosport.validate('date', dateValidator, { message: 'endDate should be after startDate' });
+    function dateValidator(err) {
+        if (this.date >= this.finishDate) {
+            err();
+        }
+    }
 
 };
--- ps-server/server/models/team.js
+++ ps-server/server/models/team.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = function(Team) {
+    Team.validatesLengthOf('name', {max: 100, message: {max: 'Name is too long'}});
 
 };
